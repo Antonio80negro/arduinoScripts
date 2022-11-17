f5e62bd3a6f151e16e3f79df01e08b4ceeab082b
Andronio <antonio80.negro@gmail.com>
2022-11-17 21:02:06 +0100

    Add files via upload

---
+++ Termometro.ino
@@ -0,0 +1,46 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int sensorPin = A0;
+
+void setup() {
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  lcd.print("by Andronio 2014");
+  Serial.println();
+  Serial.println("Termometro 1.0 - by Andronio 2014");
+  lcd.setCursor(0,1);
+  for (int puntino = 0; puntino<16; puntino++) {
+    lcd.print(".");
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println();
+}
+
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  float sensorVolt = (sensorVal/1024.0) * 5.0;
+  float temperature = (sensorVolt - .5) * 100;
+  float tempmed = temperature;
+  
+  for (int contatore = 0; contatore<10; contatore++) {
+    sensorVal = analogRead(sensorPin);
+    sensorVolt = (sensorVal/1024.0) * 5.0;
+    temperature = (sensorVolt - .5) * 100;
+    tempmed = (tempmed + temperature);
+    delay(100);
+  }
+  
+  temperature = tempmed/10;
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura:");
+  lcd.setCursor(0,1);
+  lcd.print(temperature);
+  lcd.print(" gradi");
+  Serial.print("La temperatura rilevata e' ");
+  Serial.print(temperature);
+  Serial.println(" gradi Celsius.");
+  //delay(1000);
+}
---
+++ Traffic_light.ino
@@ -0,0 +1,86 @@
+/*
+  Traffic Light
+  
+  You can switch lights OFF, sending '0' (zero) on the serial port;
+  You can switch ligths ON again, sending '1' (one) on the serial port.
+    
+  by Antonio Negro (antonio80.negro@gmail.com)
+ */
+
+
+void setup() {
+  Serial.begin(9600);
+  for(int PINcounter = 2; PINcounter<5; PINcounter++) {
+   pinMode(PINcounter, OUTPUT);
+   digitalWrite(PINcounter, LOW);
+  }
+  pinMode(9, INPUT);
+}
+
+void LightOff() {
+  int accendi = 0;
+  delay(200);
+  Serial.println("Traffic light has been switched OFF");
+      for(int PINcounter = 2; PINcounter<5; PINcounter++) {
+      digitalWrite(PINcounter, LOW);
+    }
+  int alternatore = 0;
+  long previousMillis = millis();
+  do {
+    if (Serial.read()=='1' || digitalRead(9) == HIGH) {
+        accendi = 1;
+        delay(200);
+
+    }
+    else {
+       if (alternatore == LOW && millis() > previousMillis + 500) {
+          alternatore = HIGH;
+          previousMillis = millis();
+        }
+        else if (alternatore == HIGH && millis() > previousMillis + 500) {
+          alternatore = LOW;
+          previousMillis = millis();
+         };
+         digitalWrite(3,alternatore);
+    }
+  } while (accendi != 1); // controllare 9 (0 o 1?)
+  Serial.println("Traffic light is now ON");
+  for (int PINcounter = 2; PINcounter < 5; PINcounter++) {
+    digitalWrite(PINcounter,LOW);
+  }
+}
+
+void loop() {
+ digitalWrite(4, HIGH);   // turn RED LED on (HIGH is the voltage level)
+ Serial.println("Traffic Light --> RED");
+ long PreviousMillis = millis();
+ while (millis() < PreviousMillis + 5000) {     //wait 5 seconds
+   if(Serial.read() == '0' || digitalRead(9)== HIGH) { 
+     LightOff();
+     goto RESET;
+   }
+ }
+ digitalWrite(4, LOW);    // turn RED LED off
+ digitalWrite(2, HIGH);   //turn GREEN LED on
+ Serial.print("Traffic Light --> GREEN");  
+ PreviousMillis = millis();
+ while (millis() < PreviousMillis + 4500) {
+   if(Serial.read() == '0' || digitalRead(9)== HIGH) {
+     Serial.println();
+     LightOff();
+     goto RESET;
+   }
+ }
+ digitalWrite(3, HIGH);    // turn YELLOW LED on
+ Serial.println("... YELLOW");
+ PreviousMillis = millis();
+ while (millis() < PreviousMillis + 1500) {
+   if(Serial.read() == '0' || digitalRead(9)== HIGH) { //
+     LightOff();
+     goto RESET;
+   }
+ } 
+ digitalWrite(2, LOW);
+ digitalWrite(3, LOW);    //turn GREEN and YELLOW LED off
+RESET:;
+}
